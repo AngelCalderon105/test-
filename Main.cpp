22d2d03fa0221784fc98b01213f487e842e135d4
AngelCalderon105 <89942462+AngelCalderon105@users.noreply.github.com>
2022-10-02 02:43:48 -0700

    new add

--- Main.cpp
+++ Main.cpp
@@ -4,10 +4,9 @@ using namespace std;
 int main()
 {
      int x = 0;
-<<<<<<< HEAD
+
      int y = 23;
      //hello test
-=======
      //THIS IS AN IMPORTANT CHANGE TO THE MASTER BRANCH
->>>>>>> master
+
 }
